b90fcf74dcd3ee91d9dbc697a79e408a61bfe7d7
fengfu <fengfu.qu@qunar.com>
2018-07-29 00:19:35 +0800

    add vjtop process

--- load/menu_load.sh
+++ load/menu_load.sh
@@ -9,6 +9,15 @@ echo "4.生成飞行记录JFR(10分钟)"
 echo "0.返回上级菜单"
 read -p "请输入功能序号:" num
 
+is_number(){
+  regex='^[0-9]+$'
+  if ! [[ $1 =~ $regex ]] ; then
+    echo "false"
+  else
+    echo "true"
+  fi
+}
+
 if [ $num -eq '0' ];then
   sh ../jtoolkit.sh
 elif [ $num -eq '1' ];then
@@ -19,7 +28,20 @@ elif [ $num -eq '1' ];then
 
   sudo ./show_busy_threads_with_percent.sh
 elif [ $num -eq '2' ];then
-  echo "敬请期待"
+  read -p "请输入PID或:进程路径关键字:" process
+
+  is_num=`is_number $process`
+  if [[ $is_num -eq 'false' ]]; then
+    pid=`ps aux |grep "java"|grep "$process"|grep -v "grep"|awk '{ print $2}'`
+  else
+    pid=process
+  fi
+
+  if [ ! -d "vjtop" ]; then
+    wget --no-check-certificate http://repo1.maven.org/maven2/com/vip/vjtools/vjtop/1.0.1/vjtop-1.0.1.zip && unzip vjtop-1.0.1.zip
+  fi
+  cd vjtop
+  
 elif [ $num -eq '3' ];then
   echo "敬请期待"
 elif [ $num -eq '4' ];then
