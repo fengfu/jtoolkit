036b2cdd63c29ab8a2bcdc15d46c1e28a17abae6
fengfu <fengfu.qu@qunar.com>
2018-08-03 15:34:57 +0800

    根据perm区使用率对permSize配置进行检查

--- jvm/jvm_option_check.sh
+++ jvm/jvm_option_check.sh
@@ -11,7 +11,6 @@ is_number(){
   fi
 }
 
-
 read -p "请输入PID或进程路径关键字:" process
 
 is_num=`is_number $process`
@@ -31,7 +30,7 @@ vm_line=`sudo -u $user jcmd $pid VM.command_line|grep jvm_args`
 #获取VM.version
 vm_version=`sudo -u $user jcmd $pid VM.version|grep JDK|awk '{print $2}'`
 #获取大版本
-ver=`${vm_version:0:1}`
+ver=${vm_version:0:1}
 
 #idx=`expr index $vm_line "-XX:+DisableExplicitGC"`
 result="==========JVM参数建议=========="
@@ -39,25 +38,43 @@ if [[ $vm_line = *"-XX:+DisableExplicitGC"* ]]; then
   result="$result请将DisableExplicitGC参数替换为ExplicitGCInvokesConcurrent\n"
 fi
 
+has_permSize='0'
+has_maxPermSize='0'
+
 #JDK 1.8及以上参数有效性检查
 if [[ $ver -ge '8' ]]; then
   if [[ $vm_line = *"-XX:PermSize"* ]]; then
     result="$result\nPermSize已废弃,请替换为MetaspaceSize"
+    has_permSize='1'
   fi
   if [[ $vm_line = *"-XX:MaxPermSize"* ]]; then
     result="$result\nMaxPermSize已废弃,请替换为MaxMetaspaceSize"
+    has_maxPermSize='1'
   fi
 fi
 
 #获取gc概况
-gc_stat=`(sudo -u $user jstat -gcutil $pid | awk 'NR>1 {print $0}')`
+gc_str=`(sudo -u $user jstat -gcutil $pid | awk 'NR>1 {print $0}')`
+gc_stat=($gc_str)
 ygc="0"
 ygct="0"
 fgc="0"
 fgct="0"
-perm_u="0"
+meta_u="0"
 
-perm_u=${gc_stat[4]}
+meta_u=${gc_stat[4]}
+#检查Metaspace区使用率
+meta_result=`echo "$meta_u < 40"|bc -lq`
+#perm/metaSpace使用率低于40%,检查是否显示设置了permSize/metaspaceSize
+if [[ $meta_result -eq '1' ]]; then
+  if [[ $has_permSize -eq '1' ]]; then
+    result="$result\nPerm区使用率低于40%,建议调小Perm区大小"
+  fi
+  if [[ $vm_line = *"-XX:MetaspaceSize"* ]]; then
+    result="$result\nMetaSpace使用率低于40%,建议调小MetaSpace大小"
+  fi
+fi
+
 if [[ $ver -ge '8' ]]; then
   ygc=${gc_stat[6]}
   ygct=${gc_stat[7]}
@@ -70,11 +87,14 @@ else
   fgct=${gc_stat[8]}
 fi
 
+#检查FullGC平均时间
 if [[ $fgc -gt '0' ]]; then
-  fgc_avgt=`bc <<< 'scale=1; $fgct/$fgc'`
-  if [[ $fgc_avgt -gt '1' ]]; then
+  fgc_avgt=`echo "scale=1; $fgct/$fgc"|bc -l`
+  fgc_result=`echo "$fgc_avgt > 1"|bc -lq`
+  if [[ $fgc_result -eq '1' ]]; then
     result="$result\nFullGC平均时间超过1秒,请注意优化."
   fi
 fi
 
+result="$result\n\n"
 printf $result
