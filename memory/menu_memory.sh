9dea10f1aa344b204c5a793f2f9ccb1c2db3660c
fengfu <fengfu.qu@qunar.com>
2018-08-20 17:16:02 +0800

    解决输入n脚本退出的问题

--- memory/menu_memory.sh
+++ memory/menu_memory.sh
@@ -66,34 +66,32 @@ elif [ $num -eq '2' ];then
 elif [ $num -eq '3' ];then
 
   read -p "此功能会导致Java应用长时间停顿,请确保应用已处于下线状态.是否继续？[y/n]" yesno
-  if [[ $yesno -eq 'n' ]]; then
-    return 0
-  fi
+  if [[ $yesno -eq 'y' ]]; then
+    read -p "请输入PID或进程路径关键字:" process
 
-  read -p "请输入PID或进程路径关键字:" process
-
-  is_num=`is_number $process`
-  if [[ $is_num -eq 'false' ]]; then
-    #根据进程关键字获取pid
-    pid=`ps aux |grep "java"|grep "$process"|grep -v "grep"|awk '{ print $2}'`
-  else
-    pid=process
-  fi
-
-  fname="/tmp/hsperfdata_$user/dump_$pid.bin"
-
-  #获取启动进程的用户名
-  user=`ps aux | awk -v PID=$pid '$2 == PID { print $1 }'`
-
-  echo "执行命令:sudo -u $user jmap -F -dump:format=b,file=$fname $pid导出堆内存..."
-  sudo -u $user jmap -F -dump:format=b,file=$fname $pid
-
-  if [ -f "$fname" ]; then
-    sudo -u $user gzip $fname
-    if [ -f "$fname.gz" ]; then
-      echo "堆内存已导出,路径为:$fname.gz"
+    is_num=`is_number $process`
+    if [[ $is_num -eq 'false' ]]; then
+      #根据进程关键字获取pid
+      pid=`ps aux |grep "java"|grep "$process"|grep -v "grep"|awk '{ print $2}'`
     else
-      echo "堆内存已导出,路径为:$fname"
+      pid=process
+    fi
+
+    fname="/tmp/hsperfdata_$user/dump_$pid.bin"
+
+    #获取启动进程的用户名
+    user=`ps aux | awk -v PID=$pid '$2 == PID { print $1 }'`
+
+    echo "执行命令:sudo -u $user jmap -F -dump:format=b,file=$fname $pid导出堆内存..."
+    sudo -u $user jmap -F -dump:format=b,file=$fname $pid
+
+    if [ -f "$fname" ]; then
+      sudo -u $user gzip $fname
+      if [ -f "$fname.gz" ]; then
+        echo "堆内存已导出,路径为:$fname.gz"
+      else
+        echo "堆内存已导出,路径为:$fname"
+      fi
     fi
   fi
 
