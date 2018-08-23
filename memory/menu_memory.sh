0fe914827127f0b0f6c4a1429dbd828be5ef71e0
fengfu <fengfu.qu@qunar.com>
2018-08-23 15:15:59 +0800

    增加大对象Top20功能

--- memory/menu_memory.sh
+++ memory/menu_memory.sh
@@ -5,7 +5,8 @@
 echo ""
 echo ">>>>>>>>>>内存工具箱<<<<<<<<<<"
 echo "1.堆内存使用情况"
-echo "2.对象实例统计Top10"
+echo "2.对象实例统计Top20"
+echo "3.大对象Top20"
 echo "3.dump堆内存"
 echo "0.返回上级菜单"
 echo "q.退出"
@@ -31,7 +32,7 @@ elif [ $num -eq '1' ];then
 
   if [[ -n "$process" ]]; then
     is_num=`is_number $process`
-    if [[ $is_num -eq 'false' ]]; then
+    if [[ $is_num == 'false' ]]; then
       #根据进程关键字获取pid
       pid=`ps aux |grep "java"|grep "$process"|grep -v "grep"|awk '{ print $2}'`
     else
@@ -52,7 +53,7 @@ elif [ $num -eq '2' ];then
 
   if [[ -n "$process" ]]; then
     is_num=`is_number $process`
-    if [[ $is_num -eq 'false' ]]; then
+    if [[ $is_num == 'false' ]]; then
       #根据进程关键字获取pid
       pid=`ps aux |grep "java"|grep "$process"|grep -v "grep"|awk '{ print $2}'`
     else
@@ -63,11 +64,32 @@ elif [ $num -eq '2' ];then
     user=`ps aux | awk -v PID=$pid '$2 == PID { print $1 }'`
 
     echo "执行命令:sudo -u $user jmap -histo:live $pid"
-    sudo -u $user jmap -histo:live $pid | awk 'NR<14 {print $0}'
+    sudo -u $user jmap -histo:live $pid | awk 'NR<24 {print $0}'
   fi
 
   source ./menu_memory.sh
 elif [ $num -eq '3' ];then
+  read -p "请输入PID或进程路径关键字:" process
+
+  if [[ -n "$process" ]]; then
+    is_num=`is_number $process`
+    if [[ $is_num == 'false' ]]; then
+      #根据进程关键字获取pid
+      pid=`ps aux |grep "java"|grep "$process"|grep -v "grep"|awk '{ print $2}'`
+    else
+      pid=process
+    fi
+
+    #获取启动进程的用户名
+    user=`ps aux | awk -v PID=$pid '$2 == PID { print $1 }'`
+
+    echo "执行命令:sudo -u $user jmap -histo:live $pid"
+    printf "数量\t\t空间\t\t尺寸\t\t名称\n"
+    sudo -u $user jmap -histo:live $pid | awk 'NR>3 {print $2"\t\t"$3"\t\t"$3/$2"\t\t"$4}'|sort -gr -k3| awk 'NR<21 {print $0}'
+  fi
+
+  source ./menu_memory.sh
+elif [ $num -eq '4' ];then
 
   read -p "此功能会导致Java应用长时间停顿,请确保应用已处于下线状态.是否继续？[y/n]" yesno
   if [[ $yesno = 'y' ]]; then
@@ -75,7 +97,7 @@ elif [ $num -eq '3' ];then
     if [[ -n "$process" ]]; then
 
       is_num=`is_number $process`
-      if [[ $is_num -eq 'false' ]]; then
+      if [[ $is_num == 'false' ]]; then
         #根据进程关键字获取pid
         pid=`ps aux |grep "java"|grep "$process"|grep -v "grep"|awk '{ print $2}'`
       else
