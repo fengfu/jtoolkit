eff30637cb4e8012a420cd2f8346ca33e265ec2c
fengfu <fengfu.qu@qunar.com>
2019-03-19 15:49:19 +0800

    实现通过gceasy.io分析gc.log功能

--- gc/sub_menu.sh
+++ gc/sub_menu.sh
@@ -2,37 +2,51 @@
 # GC工具箱入口
 # @author: qufengfu@gmail.com
 
+function get_json_value(){
+  local json=$1
+  local key=$2
+
+  if [[ -z "$3" ]]; then
+    local num=1
+  else
+    local num=$3
+  fi
+
+  local value=$(echo "${json}" | awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'${key}'\042/){print $(i+1)}}}' | tr -d '"' | sed -n ${num}p)
+
+  echo ${value}
+}
+
 echo ""
 echo ">>>>>>>>>>GC工具<<<<<<<<<<"
-echo "1.FullGC时间久"
-echo "2.FullGC不断"
-echo "3.FullGC频繁"
-echo "4.YoungGC时间久"
-echo "5.远程分析gc.log"
+echo "1.使用gceasy.io分析gc.log"
 echo "0.返回上级菜单"
 echo "q.退出"
 echo ""
 
 read -p "请输入:" num
 
-if [[ $num == 'q' ]]; then
+if [[ "$num" == 'q' ]]; then
   echo "Goodbye"
-elif [[ $num -eq '0' ]]; then
+elif [[ "$num" == '0' ]]; then
   cd ..
   source ./jtoolkit.sh
-elif [ $num -eq '1' ];then
-  echo "开发中,敬请期待......"
-  source ./jtoolkit.sh
-elif [ $num -eq '2' ];then
-  echo "开发中,敬请期待......"
-  source ./jtoolkit.sh
-elif [ $num -eq '3' ];then
-  echo "开发中,敬请期待......"
-  source ./jtoolkit.sh
-elif [ $num -eq '4' ];then
-  echo "开发中,敬请期待......"
-  source ./jtoolkit.sh
-elif [ $num -eq '5' ];then
-  echo "开发中,敬请期待......"
-  source ./jtoolkit.sh
+elif [ "$num" == '1' ];then
+  read -p "请输入gc.log文件全路径:" gc_file
+
+  if [[ -n "$gc_file" ]]; then
+    if [ ! -f "$gc_file" ]; then
+      echo "$gc_file文件不存在"
+    else
+      result = `curl -X POST --data-binary @$gc_file https://api.gceasy.io/analyzeGC?apiKey=9c4dc240-d620-4e4c-8369-ef4d6e5c6019 --header "Content-Type:text"`
+      url = `get_json_value $result graphURL`
+      if [[ $url = 'http'* ]]
+        echo "请将后面的URL粘贴到浏览器查看结果\n$url"
+      else
+        echo "无法获取分析结果:\n$result"
+      fi
+    fi
+  fi
+
+  source ./sub_menu.sh
 fi
