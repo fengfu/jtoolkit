4a200fd7bdbd0a7c2edb348c501961f3468dc019
fengfu <fengfu.qu@qunar.com>
2019-03-20 16:52:10 +0800

    修改获取curl结果方式

--- gc/sub_menu.sh
+++ gc/sub_menu.sh
@@ -31,7 +31,8 @@ elif [ "$num" == '1' ];then
     if [ ! -f "$gc_file" ]; then
       echo "$gc_file文件不存在"
     else
-      result=`curl -sX POST --data-binary @$gc_file https://api.gceasy.io/analyzeGC?apiKey=9c4dc240-d620-4e4c-8369-ef4d6e5c6019 --header "Content-Type:text"`
+      echo "正在上传文件进行分析，请稍候......"
+      result=$(curl -sX POST --data-binary @$gc_file https://api.gceasy.io/analyzeGC?apiKey=9c4dc240-d620-4e4c-8369-ef4d6e5c6019 --header "Content-Type:text")
       if [[ ! -n "$result" ]]; then
         has_jq=`has_command jq`
         if [[ $has_jq == 'false' ]]; then
